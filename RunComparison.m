5e90cb07f0c860d7fb92e483a1498abfd2b694f4
Matej Moravec <matejmoravec19@gmail.com>
2024-12-09 14:45:50 +0100

    Modified code to run all algorithms on the same benchmark (and storing results) in one loop

--- RunComparison.m
+++ RunComparison.m
@@ -3,72 +3,66 @@ clear all;close all;clc;
 nowPath = mfilename('fullpath');
 projectPath = nowPath(1:max(strfind(nowPath,'\'))-1);
 addpath(genpath(projectPath));
-%% ********Selecting Algorithm & Benchmark********
-%AlgorithmName = 'AMPDE';
-BenchmarkName = 'MPB';
+%% ********Selecting Benchmark********
+BenchmarkName = 'GMPB';
 %% Get the algorithm and benchmark lists
-AlgorithmsFloder = dir([projectPath,'\Algorithm']);
-AlgorithmsList = repmat("",length(AlgorithmsFloder)-2,1);
-for i = 3:length(AlgorithmsFloder)
-    AlgorithmsList(i-2,1) = AlgorithmsFloder(i).name;
+AlgorithmsFolder = dir([projectPath,'\Algorithm']);
+AlgorithmsList = repmat("",length(AlgorithmsFolder)-2,1);
+for i = 3:length(AlgorithmsFolder)
+    AlgorithmsList(i-2,1) = AlgorithmsFolder(i).name;
 end
-BenchmarksFloder = dir([projectPath,'\Benchmark']);
-BenchmarksList = repmat("",length(BenchmarksFloder)-5,1);
+BenchmarksFolder = dir([projectPath,'\Benchmark']);
+BenchmarksList = repmat("",length(BenchmarksFolder)-5,1);
 BenchmarksCount = 0;
-for i = 3:length(BenchmarksFloder)
-    if(isempty(strfind(BenchmarksFloder(i).name,'.m')))
+for i = 3:length(BenchmarksFolder)
+    if(isempty(strfind(BenchmarksFolder(i).name,'.m')))
         BenchmarksCount = BenchmarksCount + 1;
-        BenchmarksList(BenchmarksCount,1) = BenchmarksFloder(i).name;
+        BenchmarksList(BenchmarksCount,1) = BenchmarksFolder(i).name;
     end
 end
-%if(~ismember(AlgorithmName,AlgorithmsList))
-%    error("No Such Algorithm in EDOLAB");
 if(~ismember(BenchmarkName,BenchmarksList))
     error("No Such Benchmark in EDOLAB");
 end
 %% ********Benchmark parameters and Run number********
-PeakNumber                     = 10;    % The default value is 10
+PeakNumber                     = 5;     % The default value is 10
 ChangeFrequency                = 5000;  % The default value is 5000
 Dimension                      = 5;     % The default value is 5. It must be set to 2 for using Education module
 ShiftSeverity                  = 1;     % The default value is 1
-EnvironmentNumber              = 3;     % The default value is 100
+EnvironmentNumber              = 100;   % The default value is 100
 RunNumber                      = 31;    % It should be set to 31.
 SampleInterval                 = 1000;  % Comparison parameter
 %% ********Figures and Outputs********
-GeneratingExcelFile            = 1; % Set to 1 to save the output statistics in an Excel file (in the Results folder), 0 otherwise. 
-OutputFig                      = 1; % Set to 1 to draw offline error over time and current error plot, 0 otherwise.
 VisualizationOverOptimization  = 0; % This must be set to 0 if the user intends to use the Experimentation module.
-%% Running the chosen algorithm on the chosen benchmark
-for i = 1:size(AlgorithmsList)
-    main_EDO = str2func(['main_',char(AlgorithmsList(i))]);
-    [fitnesses,Problem,E_bbc,E_o,CurrentError,VisualizationInfo,Iteration] = main_EDO(VisualizationOverOptimization,PeakNumber,ChangeFrequency,SampleInterval,Dimension,ShiftSeverity,EnvironmentNumber,RunNumber,BenchmarkName);
+%% Run all algorithms on the chosen benchmark
+for i = 1:size(AlgorithmsList,1)
+    %% Running the chosen algorithm on the chosen benchmark
+    AlgorithmName = AlgorithmsList(i);
+    main_EDO = str2func(['main_',char(AlgorithmName)]);
+    [fitnesses,~,E_bbc,E_o,T_r,~,~,~] = main_EDO(VisualizationOverOptimization,PeakNumber,ChangeFrequency,SampleInterval,Dimension,ShiftSeverity,EnvironmentNumber,RunNumber,BenchmarkName);
     %% Output
-    close;clc;
     disp(['Offline error ==> ', ' Mean = ', num2str(E_o.mean), ', Median = ', num2str(E_o.median), ', Standard Error = ', num2str(E_o.StdErr)]);
-                            (['Average error before change ==> ', ' Mean = ', num2str(E_bbc.mean), ', Median = ', num2str(E_bbc.median), ', Standard Error = ', num2str(E_bbc.StdErr)]);
-    
-    %% Generating text files containing fitness values for all runs by selected evaluation for EDOAs comparison.
-    folderPath = ['D:\EDOLAB-MATLAB', '\Results\Comparison\DeleteAfterTest'];
-    cd(folderPath);
+    disp(['Average error before change ==> ', ' Mean = ', num2str(E_bbc.mean), ', Median = ', num2str(E_bbc.median), ', Standard Error = ', num2str(E_bbc.StdErr)]);
+    disp(['Runtime ==> ', ' Mean = ', num2str(T_r.mean), 's, Median = ', num2str(T_r.median), 's, Standard Error = ', num2str(T_r.StdErr), 's']);
+    %% Generating text files containing fitness values for all runs by selected evaluation for comparison
+    folderPath = fullfile(projectPath, "Results", "Comparison", "CEC2024");
     folderName = [BenchmarkName, '_Peaks', num2str(PeakNumber), '_ChangeFrequency', num2str(ChangeFrequency), '_D', num2str(Dimension), '_ShiftSeverity', num2str(ShiftSeverity), '_Environments', num2str(EnvironmentNumber)];
-    if ~exist(folderName, 'dir')
-        mkdir(folderName);
+    fullFolderPath = fullfile(folderPath, folderName);
+    if ~exist(fullFolderPath, 'dir')
+        mkdir(fullFolderPath);
     end
-    cd(folderName);
     numCols = size(fitnesses, 2);
     evaluationNumber = SampleInterval;
     col = 1;
     while col <= numCols
+        filename = [char(AlgorithmName), '_', BenchmarkName, 'Eval', num2str(evaluationNumber), '.txt'];
+        fullFilePath = fullfile(fullFolderPath, filename);
+        SaveAlgorithmResults(fullFilePath, fitnesses(:, col));
+        col = col + 1;
+        % Check if an additional save is needed (after environment change)
         if mod(evaluationNumber, ChangeFrequency) == 0 && evaluationNumber ~= ChangeFrequency * EnvironmentNumber
-            filename = [char(AlgorithmsList(i)), '_', BenchmarkName, 'Eval', num2str(evaluationNumber), '.txt'];
-            SaveAlgorithmResults(filename, fitnesses(:, col));
-            col = col + 1;
-            filename = [char(AlgorithmsList(i)), '_', BenchmarkName, 'Eval', num2str(evaluationNumber+1), '.txt'];
-            SaveAlgorithmResults(filename, fitnesses(:, col));
-            col = col + 1;
-        else
-            filename = [char(AlgorithmsList(i)), '_', BenchmarkName, 'Eval', num2str(evaluationNumber), '.txt'];
-            SaveAlgorithmResults(filename, fitnesses(:, col));
+            filename = [char(AlgorithmName), '_', BenchmarkName, 'Eval', num2str(evaluationNumber+1), '.txt'];
+            fullFilePath = fullfile(fullFolderPath, filename);
+            SaveAlgorithmResults(fullFilePath, fitnesses(:, col));
             col = col + 1;
         end
         evaluationNumber = evaluationNumber + SampleInterval;
--- Utility/Comparison/SaveAlgorithmResults.m
+++ Utility/Comparison/SaveAlgorithmResults.m
@@ -1,5 +1,8 @@
 function SaveAlgorithmResults(filename, fitnesses)
 fileId = fopen(filename, 'w');
+if fileId == -1
+    error('Failed to open file for writing: %s', filename);
+end
 fprintf(fileId, '%.13f\n', fitnesses);
 fclose(fileId);
 end
